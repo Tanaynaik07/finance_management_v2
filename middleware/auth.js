323eae92825c3a0539691706a57e9454a7624a38
Tanay Naik <tanaynaik0710@gmail.com>
2026-09-23 23:46:02 +0530

    changed routing to landing page

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,7 +6,7 @@ function ensureAuthenticated(req, res, next) {
 
 function ensureGuest(req, res, next) {
   if (!req.isAuthenticated()) return next();
-  res.redirect("/dashboard");
+  res.redirect("/");
 }
 
 module.exports = { ensureAuthenticated, ensureGuest };
